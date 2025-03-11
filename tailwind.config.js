e16bbe1a8d8f751c1055b1b723f5b56d61430cc7
safu2024 <safusalam2009@gmail.com>
2025-03-11 23:17:20 +0400

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,.tsx}",
+    "./components/**/*.{js,ts,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
 
+// module.exports = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,.tsx}",
+//     "./components/**/*.{js,ts,jsx,ts,tsx}",
+//   ],
+//   // other configurations
+// };
